028c57ebc389b8513d04bbe9dfc936b14d9c6ba5
Wenjin Kuang <wenkuajin@gmail.com>
2022-11-22 23:01:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, abort, session
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
 import os
 import datetime
 import uuid
